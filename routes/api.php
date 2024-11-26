5bdc6f699eb1322ebab3d289a26fb2594488400c
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-11-26 19:26:01 +0100

    Registro de usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('blog', PostController::class)
